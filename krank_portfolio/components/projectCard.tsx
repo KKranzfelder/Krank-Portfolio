e053bae0911f3a8994b648c2aaa70e51c227b054
kkranzfelder <kkranzfelder@gmail.com>
2022-09-30 21:52:08 -0400

    Further experimented with tailwind and styling. Tried to give cards randomly asigned colors, however there seems to be an issue with the way it is verified in the life cycle of Next.js.

--- krank_portfolio/components/projectCard.tsx
+++ krank_portfolio/components/projectCard.tsx
@@ -1,6 +1,15 @@
 const ProjectCard = function () {
+
+  const colorOptions = ['red', 'orange', 'blue', 'purple'];
+
+  const chosenOption = ((colors) => {
+    return colorOptions[Math.floor(Math.random() * colors.length)];
+  })(colorOptions);
+
+  console.log(chosenOption);
+
   return (
-    <div className='h-72 w-1/4 rounded-md bg-opacity-80 dark:bg-slate-800  dark:text-slate-400 dark:bg-opacity-80'>
+    <div className={`h-72 w-1/4 m-2 rounded-md bg-opacity-80 bg-${chosenOption}-300 dark:bg-slate-800  dark:text-slate-400 dark:bg-opacity-80 hover:mix-blend-hard-light`}>
       <span>ProjectCard</span>
     </div>
   )
--- krank_portfolio/pages/index.tsx
+++ krank_portfolio/pages/index.tsx
@@ -5,7 +5,7 @@ import ProjectCard from '../components/projectCard';
 
 const Home: NextPage = function () {
   return (
-    <div className='w-screen h-screen dark:bg-slate-900 backdrop-blur-sm -z-10 absolute dark:backdrop-blur-sm'>
+    <div className='w-screen h-screen dark:bg-slate-900 -z-10 absolute '>
       <div className='w-4/5 h-72 m-auto rounded shadow-sm bg-red-300 p-10 self-center bg-opacity-80 z-10  dark:bg-slate-800 dark:bg-opacity-80 '>
         <h2 className='text-5xl text-white dark:text-slate-300 m-5'>Welcome!</h2>
         <p className='first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-white first-letter:mr-3 first-letter:float-left dark:text-slate-400'>
@@ -15,7 +15,7 @@ const Home: NextPage = function () {
           <div className="bg-[url('../public/selfPortrait.jpg')] bg-no-repeat bg-cover sticky bottom-72 w-full h-72 overflow-hidden -z-[1] m-auto"/>
           </div>
         </div>
-      <div className='first-letter:text-white first-letter:font-bold text-slate-400 first-letter:text-5xl tracking-wide uppercase bg-slate-800 w-4/5 mx-auto mt-24 z-10 text-bold'>
+      <div className='first-letter:text-white first-letter:font-bold dark:text-slate-400 first-letter:text-5xl tracking-wide uppercase dark:bg-slate-800 w-4/5 mx-auto mt-24 z-10 text-bold bg-orange-300'>
           <span>Check out some of my</span>
           <span className='text-7xl text-white'> projects</span>
       </div>
