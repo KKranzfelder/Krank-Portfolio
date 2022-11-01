76a1fa9c557cbf43810b63cd200979c082cfb405
kkranzfelder <kkranzfelder@gmail.com>
2022-11-01 00:52:23 -0400

    experimented with adding custom animation and svgs to page. Trying to figure out how to keep them aligned for wave effect.

--- krank_portfolio/pages/index.tsx
+++ krank_portfolio/pages/index.tsx
@@ -5,7 +5,7 @@ import ProjectCard from '../components/projectCard';
 
 const Home: NextPage = function () {
   return (
-    <div className='w-screen h-screen dark:bg-slate-900 -z-10 absolute scroll-smooth md:scroll-auto'>
+    <div className='w-screen h-screen dark:bg-slate-900 -z-10 relative scroll-smooth md:scroll-auto'>
       <div className='w-4/5 h-72 m-auto rounded shadow-sm bg-red-300 p-10 self-center bg-opacity-80 z-10  dark:bg-slate-800 dark:bg-opacity-80 '>
         <h2 className='text-5xl text-white dark:text-slate-300 m-5'>Welcome!</h2>
         <p className='first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-white first-letter:mr-3 first-letter:float-left dark:text-slate-400'>
@@ -23,6 +23,31 @@ const Home: NextPage = function () {
         <ProjectCard cardNum={0}/>
         <ProjectCard cardNum={1}/>
         <ProjectCard cardNum={2}/>
+      </div>
+
+      <div className="relative bottom-0 w-4/5 h-1/6 overflow-hidden m-auto">
+        <div classname="absolute w-full rotate-180">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="fill-orange-300 animate-waveSlowest">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"/>
+          </svg>
+        </div>
+        <div classname="absolute w-full rotate-180">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="fill-red-300 animate-waveSlow">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"/>
+          </svg>
+        </div>
+
+        <div className="absolute w-full rotate-180">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="fill-purple-300 animate-wave">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"/>
+          </svg>
+        </div>
+
+        <div className="absolute w-full">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="fill-blue-300 animate-waveFast">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"/>
+        </svg>
+        </div>
       </div>
     </div>
   )
--- krank_portfolio/tailwind.config.js
+++ krank_portfolio/tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wave: {
+          '0%, 100%': { transform: 'scaleX(125%) translateX(10%)', opacity: '.5'},
+          '50%': { transform: 'scaleX(200%) translateX(-20%)', opacity: '1'}
+        }
+      },
+      animation: {
+        waveFast: 'wave 2.5s ease-in-out infinite',
+        wave: 'wave 3s ease-in-out infinite',
+        waveSlow: 'wave 3.5s ease-in-out infinite',
+        waveSlowest: 'wave 4s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 }
