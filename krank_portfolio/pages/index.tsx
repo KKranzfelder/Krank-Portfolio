2fbc0dd608051a7428c8464749099374821b4080
kkranzfelder <kkranzfelder@gmail.com>
2022-11-02 14:42:34 -0400

    Added short bio description in about as well as a dummy section for connecting to resume.

--- krank_portfolio/pages/index.tsx
+++ krank_portfolio/pages/index.tsx
@@ -23,29 +23,31 @@ const Home: NextPage = function () {
         <ProjectCard cardNum={0}/>
         <ProjectCard cardNum={1}/>
         <ProjectCard cardNum={2}/>
+        <ProjectCard cardNum={3}/>
       </div>
 
-      <div className="relative bottom-0 w-4/5 h-1/6 overflow-hidden m-auto">
-        <div classname="absolute w-full rotate-180">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="fill-orange-300 animate-waveSlowest">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"/>
-          </svg>
-        </div>
-        <div classname="absolute w-full rotate-180">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="fill-red-300 animate-waveSlow">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"/>
+      <div className="relative w-full h-1/6 overflow-hidden">
+        <div classname="absolute w-full leading-[0] bottom-0 left-0 z-10  rotate-180 m-0">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="fill-orange-300 animate-waveSlowest relative w-[100% + 1.3px] h-[259px]">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"/>
           </svg>
         </div>
 
-        <div className="absolute w-full rotate-180">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="fill-purple-300 animate-wave">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"/>
+        <div className="absolute w-full bottom-0 left-0 leading-0 z-20 rotate-180">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="fill-red-300 animate-waveSlow relative left-0 bottom-0 w-[121% + 1.3px] h-[175px]">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"/>
           </svg>
         </div>
 
-        <div className="absolute w-full">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="fill-blue-300 animate-waveFast">
-            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"/>
+        <div className="absolute w-full bottom-0 left-0 rotate-180 z-30 ">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="fill-purple-300 animate-wave relative w-[131% + 1.3px] h-[150px]">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"/>
+          </svg>
+        </div>
+
+        <div className="absolute w-full bottom-0 left-0 z-40 m-0 rotate-180">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="fill-blue-300 animate-waveFast w-[141% + 1.3px] h-[180px] relative">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"/>
         </svg>
         </div>
       </div>
--- krank_portfolio/tailwind.config.js
+++ krank_portfolio/tailwind.config.js
@@ -8,15 +8,16 @@ module.exports = {
     extend: {
       keyframes: {
         wave: {
-          '0%, 100%': { transform: 'scaleX(125%) translateX(10%)', opacity: '.5'},
-          '50%': { transform: 'scaleX(200%) translateX(-20%)', opacity: '1'}
+          '0%': { transform: 'scaleX(125%) translateX(10%)', opacity: '.5'},
+          '50%': { transform: 'scaleX(200%) translateX(-20%)', opacity: '1'},
+          '100': { transform: 'scaleX(150%) translateX(30%)', opacity: '.25'},
         }
       },
       animation: {
-        waveFast: 'wave 2.5s ease-in-out infinite',
-        wave: 'wave 3s ease-in-out infinite',
-        waveSlow: 'wave 3.5s ease-in-out infinite',
-        waveSlowest: 'wave 4s ease-in-out infinite'
+        waveFast: 'wave 2s ease-in-out infinite alternate',
+        wave: 'wave 3s ease-in-out infinite alternate',
+        waveSlow: 'wave 4s ease-in-out infinite alternate',
+        waveSlowest: 'wave 5s ease-in-out infinite alternate'
       }
     },
   },
