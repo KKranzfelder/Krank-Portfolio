41349464ede0001bd7f5f7d4bf9c903e2375c2eb
kkranzfelder <kkranzfelder@gmail.com>
2023-04-13 16:14:11 -0400

    Fixed issue of passing toggle function. Can now easily pop in and out of Resume view from top bar.

--- krank_portfolio/components/dropdown.tsx
+++ krank_portfolio/components/dropdown.tsx
@@ -5,19 +5,20 @@ const Dropdown = function() {
 
   const [resumeIsViewed, setResumeIsViewed] = useState(false);
 
-  function handleClick(e) {
+  const toggleResume = (e) => {
     e.preventDefault();
     setResumeIsViewed(!resumeIsViewed);
   }
 
+
   return (
     <>
-    {resumeIsViewed && <Resume resumeToggle={handleClick}/>}
+    {resumeIsViewed && <Resume closeResume={toggleResume}/>}
     <div className="relative h-min w-screen group trasition-transform ease-in-ease-out backdrop-blur-lg">
     <div className="bg-purple-300 h-min w-screen cursor-pointer z-50 absolute group-hover:translate-y-3 group-hover:transition-transform">
       <span className="text-white text-center block [text-shadow:2px 2px #fdba74] group-hover:hidden">Like what you see?</span>
       <div className="w-0 h-0 border-t-[6px] border-b-0 border-x-[3px] border-t-white border-x-transparent border-solid mx-auto group-hover:hidden"></div>
-      <span className="hidden group-hover:block text-center text-white text-3xl" onClick={handleClick}>Check out my Resume!</span>
+      <span className="hidden group-hover:block text-center text-white text-3xl" onClick={toggleResume}>Check out my Resume!</span>
     </div>
     <div className="bg-red-300 h-5 w-screen cursor-pointer z-40 absolute group-hover:translate-y-2 group-hover:transition-transform"></div>
     <div className="bg-orange-300 h-5 w-screen cursor-pointer z-30 absolute group-hover:translate-y-1 group-hover:transition-transform"></div>
--- krank_portfolio/components/resume.tsx
+++ krank_portfolio/components/resume.tsx
@@ -1,16 +1,11 @@
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
 
-const Resume = function (resumeToggle) {
-
-  function exit(e) {
-    e.preventDefault();
-    resumeToggle();
-  }
+const Resume = function ({ closeResume }) {
 
  return (
-  <div className={'h-full w-full bg-black bg-opacity 5 z-20'}>
+  <div className={'h-screen w-screen bg-black bg-opacity-70 z-20'}>
     <div className={'h-5 w-5 text-white'}>
-      <span onClick={exit}>x</span>
+      <span onClick={closeResume}>x</span>
     </div>
   </div>
  );
