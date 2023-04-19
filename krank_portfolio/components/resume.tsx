19d2407793ac227e944e98772b7bf0a33936c8a7
kkranzfelder <kkranzfelder@gmail.com>
2023-04-19 16:31:02 -0400

    Removed pdf-react and copy-webpack-plugin and opted for implementing a simple iFrame.

--- krank_portfolio/components/resume.tsx
+++ krank_portfolio/components/resume.tsx
@@ -1,25 +1,13 @@
-import { Document, Page } from 'react-pdf';
-import { useState } from 'react';
 
 const Resume = function ({ closeResume }) {
 
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }){
-    setNumPages(numPages);
-  }
-
  return (
   <div className={'absolute h-screen w-screen bg-black bg-opacity-70 z-20'}>
-    <div className={'h-5 w-5 text-white'}>
-      <span onClick={closeResume} className='cursor-pointer'>x</span>
-      <Document file="../assets/KaylaKranzfelderResume_11-22.pdf"
-      >
-          <Page pageNumber={pageNumber} />
-        </Document>
+    <div className={'text-white my-1 w-full'}>
+      <button onClick={closeResume} className='box-content cursor-pointer bg-red-300 rounded-sm p-0.5 w-6 float-right'>x</button>
     </div>
-  </div>
+      <iframe src='/KaylaKranzfelderResume.pdf' className='w-full min-h-full'/>
+    </div>
  );
 };
 
